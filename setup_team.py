346072e352617ee630f33cc9fb32e2225c38738e
Marie-Ève Ferland-Bernier <245264793+mferlandbernier-creator@users.noreply.github.com>
2025-12-18 16:27:54 -0500

    Premier commit : Ajout du fichier d'initialisation setup_team.py

---
+++ setup_team.py
@@ -0,0 +1,11 @@
+# setup_team.py - À exécuter par le chef de projet
+def initialiser_projet():
+	print("=== INITIALISATION DU PROJET ===")
+	print("1. Créer dépôt GitHub")
+	print("2. Cloner sur toutes les machines")
+	print("3. Créer branches :")
+	print("   - git checkout -b feature-membre1")
+	print("   - git checkout -b feature-membre2")
+	print("   - git checkout -b feature-membre3")
+	print("   - git checkout -b feature-membre4")
+	print("4. Commencer à coder !")
